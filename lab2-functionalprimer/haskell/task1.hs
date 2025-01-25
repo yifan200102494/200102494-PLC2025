8619d4016f209238e9ed44b6c187a52116ba8c13
yifan wang <200102494@aston.ac.uk>
2025-01-25 21:26:18 +0000

    修复Haskell代码错误并更新变量命名

--- lab2-functionalprimer/haskell/task1.hs
+++ lab2-functionalprimer/haskell/task1.hs
@@ -3,9 +3,9 @@ module Main where
 main =
     do
     print (myFunc1 5)
-    --print (MyFunc2 10)
-    --print (3rdFunc)
+    print (myFunc2 10)
+    print (thirdFunc)
 
 myFunc1 x = x*10
---MyFunc2 x = x*2
---3rdFunc = "Hello, this is 3rd Func"
+myFunc2 x = x*2
+thirdFunc = "Hello, this is 3rd Func"
